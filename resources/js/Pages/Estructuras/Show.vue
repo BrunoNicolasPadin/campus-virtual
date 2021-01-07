cdd3c461d9eb10f6b73a12431d8403bfc41ae991
brunoNP_ <brunonicolaspadin@gmail.com>
2021-01-07 12:21:42 -0300

    Nada importante.

--- resources/js/Pages/Estructuras/Show.vue
+++ resources/js/Pages/Estructuras/Show.vue
@@ -64,15 +64,6 @@
 
                     <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">
-                            Horarios
-                        </dt>
-                        <dd class="text-center mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
-                            <inertia-link class="hover:underline" :href="route('horarios.mostrar', [institucion_id, division.id])">Ver</inertia-link>
-                        </dd>
-                    </div>
-
-                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium text-gray-500">
                             Alumnos
                         </dt>
                         <dd class="text-center mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
@@ -80,7 +71,7 @@
                         </dd>
                     </div>
 
-                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">
                             Docentes
                         </dt>
