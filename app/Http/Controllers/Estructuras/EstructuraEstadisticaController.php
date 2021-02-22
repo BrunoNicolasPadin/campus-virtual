a1fb075babc6f2c97ab6a55e5eb27d584c5172e7
brunoNP_ <brunonicolaspadin@gmail.com>
2021-02-22 16:54:45 -0300

    DRY: estructuras.

--- app/Http/Controllers/Estructuras/EstructuraEstadisticaController.php
+++ app/Http/Controllers/Estructuras/EstructuraEstadisticaController.php
@@ -7,14 +7,20 @@ use App\Models\Asignaturas\Asignatura;
 use App\Models\CiclosLectivos\CicloLectivo;
 use App\Models\Estructuras\Division;
 use App\Models\Libretas\Libreta;
+use App\Services\Division\ObtenerPeriodosEvaluacion;
 use App\Services\FechaHora\CambiarFormatoFecha;
 use Inertia\Inertia;
 
 class EstructuraEstadisticaController extends Controller
 {
     protected $formatoService;
+    protected $divisionService;
 
-    public function __construct(CambiarFormatoFecha $formatoService)
+    public function __construct(
+        CambiarFormatoFecha $formatoService,
+        ObtenerPeriodosEvaluacion $divisionService,
+    )
+
     {
         $this->middleware('auth');
         $this->middleware('institucionCorrespondiente');
@@ -22,7 +28,7 @@ class EstructuraEstadisticaController extends Controller
         $this->middleware('divisionCorrespondiente');
 
         $this->formatoService = $formatoService;
-        
+        $this->divisionService = $divisionService;
     }
 
     public function mostrarCiclosLectivos($institucion_id, $division_id)
@@ -54,99 +60,104 @@ class EstructuraEstadisticaController extends Controller
         $libreta = Libreta::where('division_id', $division_id)->where('ciclo_lectivo_id', $ciclo_lectivo_id)->first();
         $cantidadAsignaturas = Asignatura::where('division_id', $division_id)->count();
 
-        if ($libreta && $cantidadAsignaturas > 0) {
-            if ($libreta['periodo_id'] == 1) {
-                $periodos = ['1er bimestre', '2do bimestre', '3er bimestre', '4to bimestre', 'Nota final'];
-                $contadorPeriodos = 5;
-            }
-            elseif ($libreta['periodo_id'] == 2) {
-                $periodos = ['1er trimestre', '2do trimestre', '3er trimestre', 'Nota final'];
-                $contadorPeriodos = 4;
-            }
-            elseif ($libreta['periodo_id'] == 3) {
-                $periodos = ['1er cuatrimestre', '2do cuatrimestre', 'Nota final'];
-                $contadorPeriodos = 3;
-            }
-    
-            $totalRecorrido = $cantidadAsignaturas * $contadorPeriodos;
-            $recorrido = 0;
-            $a = 0;
-    
-            $totalPeriodo = [];
-            $cantidadPeriodo = [];
-            $promedios = [];
-    
-            $totalPeriodoAlumno = [];
-            $cantidadPeriodoAlumno = [];
-            $promediosAlumnos = [];
-            $promediosMomentaneos = [];
-    
-            for ($i=0; $i < count($periodos); $i++) { 
-                $totalPeriodo[$i] = 0;
-                $cantidadPeriodo[$i] = 0;
-                $promedios[$i] = 0;
-    
-                $totalPeriodoAlumno[$i] = 0;
-                $cantidadPeriodoAlumno[$i] = 0;
-                $promediosMomentaneos[$i] = 0;
-            }
-            $i = 0;
-    
-            $libretas = Libreta::where('division_id', $division_id)
-                ->where('ciclo_lectivo_id', $ciclo_lectivo_id)
-                ->with('calificaciones', 'alumno', 'alumno.user')
-                ->get();
-            
-            foreach ($libretas as $libreta) {
-    
-                foreach ($libreta->calificaciones as $libreCali) {
-    
-                    if (!($libreCali->calificacion === null)) {
-                        $totalPeriodo[$i] = $totalPeriodo[$i] + $libreCali->calificacion;
-                        $cantidadPeriodo[$i]++;
-    
-                        $totalPeriodoAlumno[$i] = $totalPeriodoAlumno[$i] + $libreCali->calificacion;
-                        $cantidadPeriodoAlumno[$i]++;
-                    }
-                    $i++;
-                    $recorrido++;
+        if ($cantidadAsignaturas == 0) {
+            return [null, null, null];
+        }
+
+        $periodos = $this->divisionService->obtenerPeriodos($libreta);
+
+        $totalRecorrido = $cantidadAsignaturas * count($periodos);
+        $recorrido = 0;
+        $a = 0;
+
+        $totalPeriodo = [];
+        $cantidadPeriodo = [];
+        $promedios = [];
+
+        $totalPeriodoAlumno = [];
+        $cantidadPeriodoAlumno = [];
+        $promediosAlumnos = [];
+        $promediosMomentaneos = [];
+
+        for ($i=0; $i < count($periodos); $i++) { 
+            $totalPeriodo[$i] = 0;
+            $cantidadPeriodo[$i] = 0;
+            $promedios[$i] = 0;
+
+            $totalPeriodoAlumno[$i] = 0;
+            $cantidadPeriodoAlumno[$i] = 0;
+            $promediosMomentaneos[$i] = 0;
+        }
+        $i = 0;
+
+        $libretas = $this->obtenerNotasDeLaLibreta($division_id, $ciclo_lectivo_id);
+        
+        foreach ($libretas as $libreta) {
+
+            foreach ($libreta->calificaciones as $libreCali) {
+
+                if (!($libreCali->calificacion === null)) {
+                    $totalPeriodo[$i] = $totalPeriodo[$i] + $libreCali->calificacion;
+                    $cantidadPeriodo[$i]++;
+
+                    $totalPeriodoAlumno[$i] = $totalPeriodoAlumno[$i] + $libreCali->calificacion;
+                    $cantidadPeriodoAlumno[$i]++;
                 }
-    
-                $i = 0;
-    
-                if ($recorrido == $totalRecorrido) {
-    
-                    for ($i=0; $i < count($cantidadPeriodoAlumno); $i++) { 
-                        if ($cantidadPeriodoAlumno[$i] == 0) {
-                            $cantidadPeriodoAlumno[$i] = 1;
-                        }
-                        $promediosMomentaneos[$i] = \round($totalPeriodoAlumno[$i] / $cantidadPeriodoAlumno[$i], 2, PHP_ROUND_HALF_UP);
-                        
-                        $totalPeriodoAlumno[$i] = 0;
-                        $cantidadPeriodoAlumno[$i] = 0;
-                    }
-    
-                    $promediosAlumnos[$a] = [
-                        'nombre' => $libreta->alumno->user->name,
-                        'promedios' => $promediosMomentaneos,
-                    ];
-    
-                    $i = 0;
-                    $a++;
-                    $recorrido = 0;
-                }
-            }
-    
-            for ($i=0; $i < count($cantidadPeriodo); $i++) { 
-                if ($cantidadPeriodo[$i] == 0) {
-                    $cantidadPeriodo[$i] = 1;
-                }
-                $promedios[$i] = $totalPeriodo[$i] / $cantidadPeriodo[$i];
-                $promedios[$i] = \round($promedios[$i], 2, PHP_ROUND_HALF_UP);
+                $i++;
+                $recorrido++;
             }
 
-            return [$promedios, $periodos, $promediosAlumnos];
+            $i = 0;
+
+            if ($recorrido == $totalRecorrido) {
+
+                $promediosAlumnos[$a] = $this->obtenerPromediosPorAlumno($promediosMomentaneos, $cantidadPeriodoAlumno, $totalPeriodoAlumno, $libreta);
+                $a++;
+                $recorrido = 0;
+            }
         }
-        return [null, null, null];
+
+        $promedios = $this->obtenerPromedios($cantidadPeriodo, $totalPeriodo, $promedios);
+
+        return [$promedios, $periodos, $promediosAlumnos];
+    }
+
+    public function obtenerNotasDeLaLibreta($division_id, $ciclo_lectivo_id)
+    {
+        return Libreta::where('division_id', $division_id)
+        ->where('ciclo_lectivo_id', $ciclo_lectivo_id)
+        ->with('calificaciones', 'alumno', 'alumno.user')
+        ->get();
+    }
+
+    public function obtenerPromediosPorAlumno($promediosMomentaneos, $cantidadPeriodoAlumno, $totalPeriodoAlumno, $libreta)
+    {
+        for ($i=0; $i < count($cantidadPeriodoAlumno); $i++) { 
+            if ($cantidadPeriodoAlumno[$i] == 0) {
+                $cantidadPeriodoAlumno[$i] = 1;
+            }
+            $promediosMomentaneos[$i] = \round($totalPeriodoAlumno[$i] / $cantidadPeriodoAlumno[$i], 2, PHP_ROUND_HALF_UP);
+            
+            $totalPeriodoAlumno[$i] = 0;
+            $cantidadPeriodoAlumno[$i] = 0;
+        }
+
+        return [
+            'nombre' => $libreta->alumno->user->name,
+            'promedios' => $promediosMomentaneos,
+        ];
+
+    }
+
+    public function obtenerPromedios($cantidadPeriodo, $totalPeriodo, $promedios)
+    {
+        for ($i=0; $i < count($cantidadPeriodo); $i++) { 
+            if ($cantidadPeriodo[$i] == 0) {
+                $cantidadPeriodo[$i] = 1;
+            }
+            $promedios[$i] = $totalPeriodo[$i] / $cantidadPeriodo[$i];
+            $promedios[$i] = \round($promedios[$i], 2, PHP_ROUND_HALF_UP);
+        }
+        return $promedios;
     }
 }
