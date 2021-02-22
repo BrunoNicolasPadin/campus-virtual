f2fd40ef6f06bc6e19e2302876e45378a883931f
brunoNP_ <brunonicolaspadin@gmail.com>
2021-02-22 15:16:16 -0300

    DRY: asignaturas.

--- app/Http/Controllers/Asignaturas/AsignaturaEstadisticaController.php
+++ app/Http/Controllers/Asignaturas/AsignaturaEstadisticaController.php
@@ -6,15 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Models\Asignaturas\Asignatura;
 use App\Models\CiclosLectivos\CicloLectivo;
 use App\Models\Estructuras\Division;
+use App\Models\Estructuras\FormaEvaluacion;
 use App\Models\Libretas\Libreta;
+use App\Services\Division\ObtenerPeriodosEvaluacion;
 use App\Services\FechaHora\CambiarFormatoFecha;
 use Inertia\Inertia;
 
 class AsignaturaEstadisticaController extends Controller
 {
     protected $formatoService;
+    protected $divisionService;
 
-    public function __construct(CambiarFormatoFecha $formatoService)
+    public function __construct(
+        CambiarFormatoFecha $formatoService,
+        ObtenerPeriodosEvaluacion $divisionService,
+    )
+
     {
         $this->middleware('auth');
         $this->middleware('institucionCorrespondiente');
@@ -23,6 +30,7 @@ class AsignaturaEstadisticaController extends Controller
         $this->middleware('asignaturaCorrespondiente');
 
         $this->formatoService = $formatoService;
+        $this->divisionService = $divisionService;
     }
 
     public function mostrarEstadisticas($institucion_id, $division_id, $asignatura_id)
@@ -46,20 +54,11 @@ class AsignaturaEstadisticaController extends Controller
     {
         $division = Division::with('formaEvaluacion')->findOrFail($division_id);
 
-        if ($division->periodo_id == 1) {
-            $periodos = ['1er bimestre', '2do bimestre', '3er bimestre', '4to bimestre', 'Nota final'];
-        }
-        elseif ($division->periodo_id == 2) {
-            $periodos = ['1er trimestre', '2do trimestre', '3er trimestre', 'Nota final'];
-        }
-        elseif ($division->periodo_id == 3) {
-            $periodos = ['1er cuatrimestre', '2do cuatrimestre', 'Nota final'];
-        }
+        $periodos = $this->divisionService->obtenerPeriodos($division);
 
         $totalPeriodo = [];
         $cantidadPeriodo = [];
-        $promedio = [];
-        $a = 0;
+        $promedios = [];
 
         $calificacionAlumno = [];
         $calificacionesAlumnos = [];
@@ -67,52 +66,42 @@ class AsignaturaEstadisticaController extends Controller
         for ($i=0; $i < count($periodos); $i++) { 
             $totalPeriodo[$i] = 0;
             $cantidadPeriodo[$i] = 0;
-            $promedio[$i] = 0;
+            $promedios[$i] = 0;
 
             $calificacionAlumno[$i] = 0;
         }
-        $i = 0;
 
-        $libretas = Libreta::where('asignatura_id', $asignatura_id)
+        $formaEvaluacion = FormaEvaluacion::findOrFail($division->forma_evaluacion_id);
+
+        $libretas = $this->obtenerNotasDeLaLibreta($asignatura_id, $ciclo_lectivo_id);
+        $arrayTemporal = $this->recorrerLibreta($libretas, $totalPeriodo, $cantidadPeriodo, $calificacionAlumno, $calificacionesAlumnos, $formaEvaluacion);
+        $promedios = $this->obtenerPromedios($arrayTemporal[0], $arrayTemporal[1], $promedios);
+
+        return [$promedios, $periodos, $arrayTemporal[2]];
+    }
+
+    public function obtenerNotasDeLaLibreta($asignatura_id, $ciclo_lectivo_id)
+    {
+        return Libreta::where('asignatura_id', $asignatura_id)
             ->where('ciclo_lectivo_id', $ciclo_lectivo_id)
             ->with('calificaciones')
             ->get();
+    }
 
-        if ($division->formaEvaluacion->tipo == 'Escrita') {
-            foreach ($libretas as $libreta) {
-
-                foreach ($libreta->calificaciones as $libreCali) {
-    
-                    if (!($libreCali->calificacion === null)) {
-                        $calificacionAlumno[$i] = $libreCali->calificacion;
-                    }
-                    $i++;
-                }
-                $i = 0;
-    
-                $calificacionesAlumnos[$a] = [
-                    'nombre' => $libreta->alumno->user->name,
-                    'calificaciones' => $calificacionAlumno,
-                ];
-    
-                for ($i=0; $i < count($calificacionAlumno); $i++) { 
-                    $calificacionAlumno[$i] = 0;
-                }
-                $i = 0;
-    
-                $a++;
-            }
-
-            return [$promedio, $periodos, $calificacionesAlumnos];
-        }
+    public function recorrerLibreta($libretas, $totalPeriodo, $cantidadPeriodo, $calificacionAlumno, $calificacionesAlumnos, $formaEvaluacion) {
+        
+        $i = 0;
+        $a = 0;
 
         foreach ($libretas as $libreta) {
 
             foreach ($libreta->calificaciones as $libreCali) {
 
                 if (!($libreCali->calificacion === null)) {
-                    $totalPeriodo[$i] = $totalPeriodo[$i] + $libreCali->calificacion;
-                    $cantidadPeriodo[$i]++;
+                    if (!($formaEvaluacion->tipo == 'Escrita')) {
+                        $totalPeriodo[$i] = $totalPeriodo[$i] + $libreCali->calificacion;
+                        $cantidadPeriodo[$i]++;
+                    }
 
                     $calificacionAlumno[$i] = $libreCali->calificacion;
                 }
@@ -133,13 +122,17 @@ class AsignaturaEstadisticaController extends Controller
             $a++;
         }
 
+        return [$totalPeriodo, $cantidadPeriodo, $calificacionesAlumnos];
+    }
+
+    public function obtenerPromedios($totalPeriodo, $cantidadPeriodo, $promedios)
+    {
         for ($i=0; $i < count($cantidadPeriodo); $i++) { 
             if ($cantidadPeriodo[$i] == 0) {
                 $cantidadPeriodo[$i] = 1;
             }
-            $promedio[$i] = \round($totalPeriodo[$i] / $cantidadPeriodo[$i], 2, PHP_ROUND_HALF_UP);
+            $promedios[$i] = \round($totalPeriodo[$i] / $cantidadPeriodo[$i], 2, PHP_ROUND_HALF_UP);
         }
-
-        return [$promedio, $periodos, $calificacionesAlumnos];
+        return $promedios;
     }
 }
